ade3017890ca99c1eba3a8d4caf06d57a407fd0c
kenkov <kenkovtan@gmail.com>
2018-01-08 15:51:15 +0900

    scripts を更新

--- scripts/twitter_dialog_extractor.py
+++ scripts/twitter_dialog_extractor.py
@@ -2,13 +2,38 @@
 # coding:utf-8
 
 
+from diautils.preprocessor.twitter import Preprocessor
+from diautils.preprocessor.twitter import TwitterPreprocessor
+
+preprocessor = Preprocessor()
+twpreprocessor = TwitterPreprocessor()
+
+
+def preprocess(text):
+    funcs = [preprocessor.remove_link,
+             preprocessor.remove_newline,
+             twpreprocessor.remove_mention,
+             twpreprocessor.remove_retweet,
+             twpreprocessor.remove_tag,
+             preprocessor.shrink_cont_spaces,
+             preprocessor.strip,
+             ]
+    for func in funcs:
+        text = func(text)
+    return text
+
+
 if __name__ == '__main__':
     import sys
-    import json
     from diautils.extractor.twitter import TwitterExtractor
 
     dialog_extractor = TwitterExtractor()
+
     filename = sys.argv[1]
     for replies in dialog_extractor.extract_from_file(filename):
-        replies_json = json.dumps([tweet["text"] for tweet in replies])
-        print(replies_json)
+        replies = [tweet["text"] for tweet in replies]
+        for src, tgt in zip(replies, replies[1:]):
+            src = preprocess(src)
+            tgt = preprocess(tgt)
+            if src and tgt:
+                print(f"{src}\t{tgt}")
