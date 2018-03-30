e7d287d34cc0f7bed32364d846629d9453491def
kenkov <kenkovtan@gmail.com>
2018-03-30 15:47:34 +0900

    diautils/stats/wordcount.py を実装

---
+++ diautils/stats/wordcount.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+from collections import Counter
+
+class WordCounter:
+    def __init__(self, separator=" "):
+        self._separator = separator
+        self._counter = Counter()
+
+    @property
+    def counter(self):
+        return self._counter
+
+    def fit(self, texts):
+        self._counter = Counter()
+        for text in texts:
+            words = text.split(self._separator)
+            self._counter.update(words)
+
+
+if __name__ == "__main__":
+    import sys
+
+    counter = WordCounter()
+    counter.fit((line.strip("\n") for line in sys.stdin))
+
+    for word, cnt in sorted(counter.counter.items(),
+                            key=lambda x: x[1]):
+        print(f"{word} {cnt}")
