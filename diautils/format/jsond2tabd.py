e1dbc83b333dd690be3ba75f6f8c5aca84d4a43c
kenkov <kenkovtan@gmail.com>
2018-06-19 23:36:37 +0900

    Implement jsond to tabd format converter

---
+++ diautils/format/jsond2tabd.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+
+"""JSON 形式対話データ
+
+    [{"text": "発話1", ...}, {"text": "発話2", ...}, {"text": "発話3", ...}, ...]
+
+
+をタブ区切り形式対話データ
+
+    発話1<TAB>発話2<TAB>発話3<TAB>...
+
+に変換するスクリプト
+"""
+
+import json
+
+
+def convert_jsond2tabd(json_str):
+    texts = [item["text"]
+             for item in json.loads(json_str)]
+    return "\t".join(texts)
+
+
+if __name__ == '__main__':
+    import sys
+    import json
+
+    for line in sys.stdin:
+        replies = [tweet["text"].replace("\n", " ") for tweet in json.loads(line)]
+        print("\t".join(replies))
