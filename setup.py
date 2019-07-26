0b92dadd33d2acfd080bd89dbc8e332321993ea6
kenkov <kenkovtan@gmail.com>
2019-07-27 02:13:33 +0900

    依存ライブラリを追加

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ setup(
     name="diautils",
     packages=["diautils", "diautils.preprocessor"],
     install_requires=[
+        "emoji==0.5.0",
+        "fire==0.1.3",
         "mojimoji==0.0.8",
         ],
-    version="0.1.0",
+    version="0.1.1",
     author="kenkov",
     author_email="kenkovtan@gmail.com",
     url="http://kenkov.jp",
